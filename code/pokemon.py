41035931abf7455bd926ffc842518d93fc5a7a57
thouay-baccam <thouay.baccam@laplateforme.io>
2024-01-26 13:24:25 +0100

    pygame fixed?

--- code/pokemon.py
+++ code/pokemon.py
@@ -9,13 +9,13 @@ class Pokemon:
         name,
         types,
         attack,
-        defense, 
-        level, 
+        defense,
+        level,
         front_sprite,
         back_sprite,
-        evolution = None,
-        evolution_level = None,
-        ):
+        evolution=None,
+        evolution_level=None,
+    ):
         self.name = name
 
         self.types = types
@@ -31,59 +31,53 @@ class Pokemon:
         self.evolution = evolution
         self.evolution_level = evolution_level
 
+    # Méthode pour changer de niveau
     def level_up(self):
         self.level += 1
         pokemons = []
         with open(save_path, "r") as file:
             pokemons = json.load(file)
             for pokemon in pokemons:
-                if pokemon['name'] == self.name:
-                    pokemon['level'] = self.level
+                if pokemon["name"] == self.name:
+                    pokemon["level"] = self.level
         with open(save_path, "w") as file:
             json.dump(pokemons, file, indent=4)
         print(f"{self.name} has leveled up! It is now level {self.level}.")
         self.check_evolution()
 
+    # Méthode pour vérifier evolution
     def check_evolution(self):
-        print(f"Checking evolution for {self.name}. Current level: {self.level}, Required level: {self.evolution_level}")
+        print(
+            f"Checking evolution for {self.name}."
+            f"Current level: {self.level},"
+            f"Required level: {self.evolution_level}"
+        )
         if self.evolution and self.level >= self.evolution_level:
             self.evolve()
 
+    # Méthode pour évoluer
     def evolve(self):
         print(f"{self.name} is evolving into {self.evolution}!")
-        # Load evolution data
         with open(pokemon_path, "r") as file:
             pokemons = json.load(file)
             evolution_data = {}
             for pokemon in pokemons:
-                if pokemon['name'] == self.evolution:
+                if pokemon["name"] == self.evolution:
                     evolution_data = pokemon
             if evolution_data:
                 self.update_pokemon(evolution_data)
             else:
                 print("Evolution data not found.")
 
+    # Méthode pour mettre à jour le pokémon
     def update_pokemon(self, data):
         print(f"Updating {self.name} to {data['name']}")
         pokemons = []
         with open(save_path, "r") as file:
             pokemons = json.load(file)
             for pokemon in pokemons:
-                if pokemon['name'] == self.name:
-                    pokemon['name'] = data['name']
-                    pokemon['types'] = data['types']
-                    pokemon['attack_stat'] = data['attack_stat']
-                    pokemon['defense_stat'] = data['defense_stat']
+                if pokemon["name"] == self.name:
+                    pokemon = data
                     pokemon['level'] = self.level
-                    if (
-                        'evolution' in data
-                        and 'evolution_level' in data
-                    ):
-                        pokemon['evolution'] = data['evolution']
-                        pokemon['evolution_level'] = data['evolution_level']
-                    else:
-                        pokemon.pop('evolution')
-                        pokemon.pop('evolution_level')
-                    break
         with open(save_path, "w") as file:
             json.dump(pokemons, file, indent=4)
